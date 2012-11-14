7521dd448308339bb30c94957bf78686e2574754
T. Andrew Binkowski <abinkowski@uchicago.edu>
2012-11-14 09:42:03 -0600

    Add comments and change the password to abc123.

--- HelloWorld/ViewController.m
+++ HelloWorld/ViewController.m
@@ -8,18 +8,30 @@
 
 #import "ViewController.h"
 
+// Class Extension (Private) ///////////////////////////////////////////////////
 @interface ViewController ()
 
 @end
 
+// Class ///////////////////////////////////////////////////////////////////////
 @implementation ViewController
 
+/*******************************************************************************
+ * @method      viewDidLoad
+ * @abstract    Method called when the view is loaded
+ * @description
+ *******************************************************************************/
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
+/*******************************************************************************
+ * @method      didReceiveMemoryWarning
+ * @abstract    Called when memory is low
+ * @description
+ *******************************************************************************/
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
@@ -32,22 +44,40 @@
  * @abstract    Tap the button to change the greeting
  * @description
  *******************************************************************************/
-- (IBAction)tapToChangeGreeting:(UIButton *)sender {
+- (IBAction)tapToChangeGreeting:(UIButton *)sender
+{
     self.greetingLabel.text = @"Goodbye";
 }
 
 #pragma mark - UITextField Protocol Methods
+
+/*******************************************************************************
+ * @method      textFieldShoudlReturn:
+ * @abstract    Method called when the "Return" button is hit on the keyboard
+ * @description
+ *******************************************************************************/
 - (BOOL)textFieldShouldReturn:(UITextField *)textField {
     
+    // Test if the textField is the password field (not necessary, but could be useful if need to distinguish between textFields such as a username field)
     if (textField == self.passwordField) {
-        if ([self.passwordField.text isEqualToString:@"asdf"]) {
+
+        // Test if the password is equal to "abc123"
+        if ([self.passwordField.text isEqualToString:@"abc123"]) {
+
+            // Update the greeting label
             self.greetingLabel.text = [NSString stringWithFormat:@"Password Accepted"];
+            
+            // Change the view controller's main view background to green
             self.view.backgroundColor = [UIColor greenColor];
         } else {
+            // The password is not correct, change the background color to red
             self.view.backgroundColor = [UIColor redColor];
+            
+            // Change the greeting to "Denied"
             self.greetingLabel.text = [NSString stringWithFormat:@"Denied"];
         }
     }
+    
     // Print the text string inputed to the console
     NSLog(@">>>> The text input: %@", self.passwordField.text);
     
